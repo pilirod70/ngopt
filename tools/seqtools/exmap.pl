90295469bde6293940d9c053c30ccd4f638eeee5
genomicist <genomicist@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2010-12-20 23:43:30 +0000

    fixed SAM file splitter for extracting mapped reads

--- tools/seqtools/exmap.pl
+++ tools/seqtools/exmap.pl
@@ -3,36 +3,71 @@
 use strict;
 use warnings;
 
+if (scalar(@ARGV) != 2) {
+	print STDOUT "Splits a set of reads into mapped and unmapped files\n";
+	print STDOUT "Usage: exmap.pl <sam_file> <reads_file>\n";
+	print STDOUT "Reads can be in either fasta or fastq format\n";
+	print STDOUT "Two output files will be created: reads_base.mapped.fasta/q and reads_base.unmapped.fasta/q\n";
+	exit 1;
+}	
+
 my %keepers;
 
 open (SAM ,"<", $ARGV[0]);
 
 while (<SAM>) {
 	chomp;
-	if (substr($_,0,1) eq "@") {
-		continue;
-	} else {
+	if ( substr($_,0,1) ne "@") {
 		my @tmp = split;
 		$keepers{$tmp[0]} = 1;
 	}
 }
 
 open (FQ, "<", $ARGV[1]);
+my $base = substr($ARGV[1],0,rindex($ARGV[1],".f[a,q]"));
+my $map_file = $base.".mapped.fasta";
+my $umap_file = $base.".unmapped.fasta";
+my $first = 1;
+my $fastq = 0;
+if (getc(FQ) == '@') {
+	$fastq = 1;
+	$map_file = $base.".mapped.fastq";
+	$umap_file = $base.".unmapped.fastq";
+}
 
+my $mapped = 0;
+my $unmapped = 0;
+open (MAP, ">", $base.".mapped.fastq");
+open (UMAP,">", $base.".unmapped.fastq");
 while (<FQ>) {
 	chomp;
 	my $line;
-	my $hdr = substr($_,1,length($_)-3);
+	my $hdr;
+	if ($first){
+		$hdr = substr($_,1,length($_)-3);
+		$first = 0;
+	}
 	if ($keepers{$hdr}) {
-		print STDOUT $_."\n";
-		print STDOUT <FQ>;
-		print STDOUT <FQ>;
-		print STDOUT <FQ>;
+		print MAP $_."\n";
+		print MAP <FQ>;
+		if ($fastq) {
+			print MAP <FQ>;
+			print MAP <FQ>;
+		}
+		$mapped++;
 	} else {
-		$line = <FQ>;
-		$line = <FQ>;
-		$line = <FQ>;
+		print UMAP $_."\n";
+		print UMAP <FQ>;
+		if ($fastq) {
+			print UMAP <FQ>;
+			print UMAP <FQ>;
+		}
+		$unmapped++;
 	}
 }
 
+close MAP;
+close UMAP;
+
+print STDOUT "Wrote $mapped to $map_file and $unmapped to $umap_file\n"; 
 
