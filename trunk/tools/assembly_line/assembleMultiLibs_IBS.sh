430e1458e34e4b44e8fe742cbe27b8a42c9d504b
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-01-07 22:49:11 +0000

    Finished multiple library assembly pipeline. Should work for any number of libraries.

--- trunk/tools/assembly_line/assembleMultiLibs_IBS.sh
+++ trunk/tools/assembly_line/assembleMultiLibs_IBS.sh
@@ -3,60 +3,58 @@
 	c="10"
 	L="150"
 	h="2.2"
-	
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 <output_base> <kcut> <mnpair> <sm_lib> <sm_ins> <lg_lib> <lg_ins>"
+	let mod2=$(($# % 2))	
+#	 [ $# -lt 5 ] ||
+if [ $# -lt 5 ] || [ $mod2 -eq 0 ] ; then
+	echo "Usage: $0 <output_base> <kcut> <mnpair> <lib1> <ins1> <lib2> <ins2> . . . <libN> <insN>"
 	echo "Libraries must be shuffled."
-	echo "Final assembly under <output_base>_2stg/scaf_lg/scaffolds_h${h}_L${L}_w<mnpair>.fasta"
+#	echo "Final assembly under <output_base>_2stg/scaf_lg/scaffolds_h${h}_L${L}_w<mnpair>.fasta"
 	exit -1
 fi
 	base=$1
 	kcut=$2
 	w=$3
 
-	a="${base}_2stg"
+	num_libs=$((( $# - 3 )/2))
+	a="${base}_${num_libs}libs"
 	if [ ! -d $a ]; then
 		mkdir $a
 	fi
-	num_libs=$(( $# - 3 ))
 	
 	stdout="$a/std.out"
 	stderr="$a/std.err"
 	echo -n > $stdout
 	echo -n > $stderr
-	echo $base	
-	sm_ins=$4
-	sm=$5
-	lg_ins=$6
-	lg=$7
-	fa_reads="$base.fasta"
+	echo $base
+	fa_reads="$base.${num_libs}libs.fasta"
 	if [ ! -f $fa_reads ]; then
 		x=4
-		while [ $x -le ($# - 1) ] ; do 
+		while [ $x -le $(($# - 1)) ] ; do 
 			cat $(( $x )) >> $fa_reads 
 			x=$(( $x + 2 ))
 		done
 	fi
 	prefix="$a/$base.idba"
+	
 	idba -r $fa_reads -o $prefix --minCount $kcut > $stdout 2> $stderr 
-	echo "Done contiging\n" >> $stdout 2>> $stderr 
+	echo "Done contiging" >> $stdout 2>> $stderr
 	rmctgs $L $prefix-contig.fa > $prefix-contig.$L.fa
 
 	contigs=$prefix-contig.$L.fa
 	x=4
 	stage=1
 	dir=$a/stage$stage
-	while [ $x -le ($# - 1) ] ; do 
-		lib=$(( $x )) >> $fa_reads 
-		ins=$(( $x + 1 ))
-		echo "Begin stage $stage of scaffolding" | tee -a $stdout $stderr
-		scaf_sopra_bwa.sh $dir $contigs $lib $w $ins >> $stdout 2>> $stderr 
+	while [ $x -le $(($# - 1)) ] ; do 
+		lib=$x >> $fa_reads 
+		ins=$[$x+1]
+		echo "Begin stage $stage of scaffolding" | tee -a $stdout $stderr 
+		scaf_sopra_bwa.sh $dir $contigs ${!lib} $w ${!ins} >> $stdout 2>> $stderr 
 		x=$(( $x + 2 ))
 		stage=$(( $stage + 1 ))
 		contigs=$dir/scaffolds_h$h\L$L\_w$w.fasta
 		dir=$a/stage$stage
 	done
-
+	echo \"Scaffolding done. Final scaffolds are in $contigs\" | tee -a $stdout $stderr
 #	echo "Begin first stage of scaffolding" | tee -a $stdout $stderr
 #	scaf_sopra_bwa.sh $a $prefix-contig.$L.fa $sm_ins $w $sm >> $stdout 2>> $stderr 
 #	echo "Begin second stage of scaffolding" | tee -a $stdout $stderr
