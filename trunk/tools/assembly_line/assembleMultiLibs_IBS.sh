61d344bbe55545d1fa8299d057fe72643f91eb7d
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-01-08 05:56:59 +0000

    changed file name

--- trunk/tools/assembly_line/assembleMultiLibs_IBS.sh
+++ trunk/tools/assembly_line/assembleMultiLibs_IBS.sh
@@ -6,7 +6,7 @@
 	let mod2=$(($# % 2))	
 #	 [ $# -lt 5 ] ||
 if [ $# -lt 5 ] || [ $mod2 -eq 0 ] ; then
-	echo "Usage: $0 <output_base> <kcut> <mnpair> <lib1> <ins1> <lib2> <ins2> . . . <libN> <insN>"
+	echo "Usage: `basename $0` <output_base> <kcut> <mnpair> <lib1> <ins1> <lib2> <ins2> . . . <libN> <insN>"
 	echo "Libraries must be shuffled."
 #	echo "Final assembly under <output_base>_2stg/scaf_lg/scaffolds_h${h}_L${L}_w<mnpair>.fasta"
 	exit -1
@@ -30,7 +30,7 @@ fi
 	if [ ! -f $fa_reads ]; then
 		x=4
 		while [ $x -le $(($# - 1)) ] ; do 
-			cat $(( $x )) >> $fa_reads 
+			cat ${!x} >> $fa_reads 
 			x=$(( $x + 2 ))
 		done
 	fi
@@ -48,10 +48,11 @@ fi
 		lib=$x >> $fa_reads 
 		ins=$[$x+1]
 		echo "Begin stage $stage of scaffolding" | tee -a $stdout $stderr 
+		echo "Executing the following command: scaf_sopra_bwa.sh $dir $contigs ${!lib} $w ${!ins} >> $stdout 2>> $stderr" 
 		scaf_sopra_bwa.sh $dir $contigs ${!lib} $w ${!ins} >> $stdout 2>> $stderr 
 		x=$(( $x + 2 ))
 		stage=$(( $stage + 1 ))
-		contigs=$dir/scaffolds_h$h\L$L\_w$w.fasta
+		contigs=$dir/scaffolds_h${h}_L${L}_w$w.fasta
 		dir=$a/stage$stage
 	done
 	echo \"Scaffolding done. Final scaffolds are in $contigs\" | tee -a $stdout $stderr
