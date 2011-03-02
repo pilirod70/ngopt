29e23fb1cebeff34379e04ec282f0c9438981ae0
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-03-02 21:01:02 +0000

    added some blast tools and fixed some gc-cov tools

--- trunk/tools/gc-cov/get_pileup.sh
+++ trunk/tools/gc-cov/get_pileup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ $# -lt 2 ] ; then
+if [ $# -lt 3 ] ; then
 	echo "Usage: `basename $0` <output_dir> <ref> <reads>"
-	echo "<reads> can be read from stdin"
 	echo "Final pileup file is printed to stdout"
 	exit
 fi
@@ -10,17 +9,14 @@ fi
 
 
 out=$1
-prefix=$out/$ref_base.bwa
 ref=$2
-ref_base=`basename $ref`
-reads=/dev/stdin
-base=$ref_base
-if [ $# -eq 3 ]; then
-	reads=$3
-	base=`basename $reads`
-fi
+prefix=$out/`basename $ref`
+reads=$3
+base=`basename $reads`
 bwa index -a is -p $prefix $ref > $out/index.out
-bwa aln $prefix $reads > $out/$base.sai
-bwa samse $prefix $out/$base.sai $reads > $out/$ref_base.sam
+bwa aln $prefix $reads 2> $out/aln.err | bwa samse $prefix - $reads 2> $out/samse.err  | samtools view -bhS - 2> $out/view.err | samtools sort -o - $out/$base.sort 2> $out/sort.err | samtools mpileup -f $ref -  
 
-samtools view -bhS $out/$ref_base.sam | samtools sort -o - $out/$ref_base | samtools pileup -f $ref -  
+#bwa aln $prefix $reads 2> $out/aln.err | bwa samse $prefix - $reads 2> $out/samse.err > $out/out.sam 
+#samtools view -bhS $out/out.sam 2> $out/view.err > $out/out.bam 
+#samtools sort -o $out/out.bam $out/out.sort 2> $out/sort.err > $out/out.sort.bam 
+#samtools mpileup -f $ref $out/out.sort.bam
