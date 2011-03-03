9ab64861881fdf81dc20cb6e4ca4e0fe7b87e640
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-03-03 06:13:04 +0000

    changed 2 scripts in gc-cov

--- trunk/tools/gc-cov/summarize_cov-gc-len.pl
+++ trunk/tools/gc-cov/summarize_cov-gc-len.pl
@@ -1,45 +1,33 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
-sub mean(\@);
-sub var(\@$);
-print "Usage: $0 <tabd_file>\n" if (scalar(@ARGV)!=1);
-exit  if (scalar(@ARGV)!=1);
+sub muvar(\@);
+print "Usage: $0 <tabd_file(s)>\n" if (scalar(@ARGV) < 2);
+exit  if (scalar(@ARGV)<2);
 
-my $file = shift;
-open(IN,"<",$file);
-my @gc;
-my @cov;
-my @len;
-
-while(<IN>){
-	chomp;
-	my @ctg = split /\t/;
-	push (@gc,$ctg[1]);
-	push (@cov,$ctg[2]);
-	push (@len,$ctg[3]);
-}
-my $gc_ = mean(@gc);
-my $cov_ = mean(@cov);
-my $len_ = mean(@len);
-
-print $file."\t".$gc_."\t".var(@gc,$gc_)."\t".$cov_."\t".var(@cov,$cov_)."\t".$len_."\t".var(@len,$len_)."\n";
-
-sub mean(\@) {
+while (@ARGV) {
+	my $file = shift;
+	open(IN,"<",$file);
+	my (@gc, @cov, @len);
+	while(<IN>){
+		chomp;
+		my @ctg = split /\t/;
+		push (@gc,$ctg[1]);
+		push (@cov,$ctg[2]);
+		push (@len,$ctg[3]);
+	}
+	print "$file\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n", (muvar(@gc),muvar(@cov),muvar(@len));
+}	
+sub muvar(\@) {
 	my $vec = shift;
 	my $sum = 0;
 	for my $i (@$vec) {
 		$sum += $i;
 	}
-	return $sum/scalar(@$vec);
-}
-
-sub var(\@$) {
-	my $vec = shift;
-	my $mean = shift;
-	my $sum = 0;
+	my $mean = $sum/scalar(@$vec);
+	$sum = 0;
 	for my $i (@$vec) {
 		$sum += ($mean-$i)**2;
 	}
-	return $sum/scalar(@$vec);
+	return ($mean, $sum/scalar(@$vec));
 }
