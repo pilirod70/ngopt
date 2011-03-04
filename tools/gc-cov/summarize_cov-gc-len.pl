9fad5d9bb3a57c2c551c1cf17b3dc6f802db7884
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-03-04 00:29:09 +0000

    fixed script to summarize gc/coverage data

--- tools/gc-cov/summarize_cov-gc-len.pl
+++ tools/gc-cov/summarize_cov-gc-len.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
-sub muvar(\@);
-print "Usage: $0 <tabd_file(s)>\n" if (scalar(@ARGV) < 2);
+use File::Basename;
+sub muvar (\@);
+print "Usage: ".basename($0)." <tabd_file(s)>\n" if (scalar(@ARGV) < 2);
 exit  if (scalar(@ARGV)<2);
 
 while (@ARGV) {
 	my $file = shift;
 	open(IN,"<",$file);
+	<IN>;
 	my (@gc, @cov, @len);
 	while(<IN>){
 		chomp;
@@ -16,11 +18,13 @@ while (@ARGV) {
 		push (@cov,$ctg[2]);
 		push (@len,$ctg[3]);
 	}
-	print "$file\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n", (muvar(@gc),muvar(@cov),muvar(@len));
+	printf "$file\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n", (muvar(@gc),muvar(@cov),muvar(@len));
 }	
-sub muvar(\@) {
+sub muvar (\@){
 	my $vec = shift;
 	my $sum = 0;
+	#print STDERR "length: ".scalar(@$vec)."\n";
+	print STDERR @$vec."\n" if (scalar(@$vec) == 1);
 	for my $i (@$vec) {
 		$sum += $i;
 	}
