dd54a0664a326a6c0477c651ea218e1e0faa7b92
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-03-22 23:08:09 +0000

    un-hardcoded "koadman" from assemblathon_sgescoring.sh, replacing it with "$USER". Now we can all use it on the SGE!

--- trunk/assemblathon/assemblathon_sgescoring.sh
+++ trunk/assemblathon/assemblathon_sgescoring.sh
@@ -9,9 +9,9 @@
 # Script to score an assembly in SGE using Mauve
 # Depends on existence of a VNC or real X server
 #
-MAUVEDIR="/home/koadman/software/mauve_2.4.0_beta/"
-MAUVE="/home/koadman/software/jre1.6.0_20/bin/java -Xmx2000m -cp Mauve.jar "
-WORKDIR="/state/partition1/koadman/assemblathon/$JOB_ID"
+MAUVEDIR="/home/$USER/software/mauve_2.4.0_beta/"
+MAUVE="/home/$USER/software/jre1.6.0_20/bin/java -Xmx2000m -cp Mauve.jar "
+WORKDIR="/state/partition1/$USER/assemblathon/$JOB_ID"
 export TMP="$WORKDIR/tmp"
 export TMPDIR="$WORKDIR/tmp"
 
