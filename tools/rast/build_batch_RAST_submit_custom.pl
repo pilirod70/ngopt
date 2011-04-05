9c13af396e500fcfdd87a809fd082cfc40f935c6
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-04-05 23:27:59 +0000

    added perl scripts for building batch scripts for submitting to RAST

---
+++ tools/rast/build_batch_RAST_submit_custom.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+
+use File::Basename;
+die "Usage: ".basename($0)." <name_file> <fasta_dir> <fasta_file_prefix> <outdir> <keep_file>\n" if (scalar(@ARGV) != 5);
+my $TPL = "--user atritt  --passwd halophile ".
+          "--genetic_code 11 --gene_caller rast --determine_family --domain Archaea";  
+my $names_file = shift;
+my $dir = shift;
+my $pfx = shift;
+my $outdir = shift;
+my $spec_file = shift;
+
+open (NAMES,"<",$names_file);
+my %name = ();
+while(<NAMES>){
+	chomp;
+	my @line = split(/\t/,$_);
+	$names{$line[0]} = $line[1];
+}
+my %keep = ();
+open(NAMES,"<",$spec_file);
+while(<NAMES>){
+	chomp;
+	$keep{$_} = 1;
+}
+
+print "#!/bin/bash\n";
+my @files = glob "$dir/*$pfx";
+for my $file (@files){
+	my $base = basename($file,$pfx);
+	next if (!defined($keep{$base}));
+	my $name = $base; 
+	if (!defined($names{$base})) {
+		print STDERR "no name found in $names_file for $base\n";	
+	} else {
+		$name = $names{$base};
+	}
+	print "svr_submit_RAST_job --fasta $file --bioname \"$name\" $TPL > $outdir/$base.out 2> $outdir/$base.err\n";
+}
+# 
+#   usage: svr_submit_RAST_job.pl  --user UserName  --passwd Passwd 
+#                                 (--genbank GenbankFilename | --fasta FASTAfilename) --domain (Bacteria|Archaea)  
+#                                 [--taxon_ID NCBI_taxonomy_ID(def:666666)]   
+#                                 [--bioname "genus species strain" (def: "Unknown sp.")]  
+#                                 [--genetic_code (11|4)] 
+#                                 [--gene_caller (rast|glimmer3)] 
+#                                 [--determine_family] 
+#                                 [--reannotate_only] [--test] [--nonActive]
+#
+
+
