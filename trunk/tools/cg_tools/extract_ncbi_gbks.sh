bbb590924321385c2f0bb2d3fd5dadba897dc2cb
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-04-07 06:15:16 +0000

    fixed Halorubrum distributum ambiguity in swap_ccid_for_fullname.sh

--- trunk/tools/cg_tools/extract_ncbi_gbks.sh
+++ trunk/tools/cg_tools/extract_ncbi_gbks.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-	echo "Usage: `basename $0` <list_file>"
+if [ $# -ne 3 ]; then
+	echo "Usage: `basename $0` <list_file> <NCBI_Genomes_dir> <output_dir>"
 	exit
 fi
-
 list=$1
+NCBI_DIR=$2
+OUT_DIR=$3
 for bug in `cat $list`; do 
- head -q -n 1 $bug/*.gbk | perl -p -i -e "s/\ +/\t/g" | cut -f 2 > $bug.2
- head -q -n 1 $bug/*.gbk | perl -p -i -e "s/\ +/\t/g" | cut -f 3 > $bug.3 
- paste $bug.3 $bug.2 | sort -g -r | cut -f2 > $bug.gbks
- rm $bug.[2,3] 
-done
-
-for gbks in *.gbks; do
- org=`basename $gbks .gbks`
- for gbk in `cat $gbks`; do 
-  cat $org/$gbk.gbk >> $org.gbk
+ echo $bug
+ head -q -n 1 $NCBI_DIR/$bug/*.gbk | perl -p -i -e "s/\ +/\t/g" | cut -f 2 > $NCBI_DIR/$bug.2
+ head -q -n 1 $NCBI_DIR/$bug/*.gbk | perl -p -i -e "s/\ +/\t/g" | cut -f 3 > $NCBI_DIR/$bug.3 
+ paste $bug.3 $bug.2 | sort -g -r | cut -f2 > $NCBI_DIR/$bug.gbks
+ rm $NCBI_DIR/$bug.[2,3] 
+ for gbk in `cat $NCBI_DIR/$bug.gbks`; do 
+  cat $NCBI_DIR/$bug/$gbk.gbk >> $NCBI_DIR/$bug.gbk
  done
- rm $gbks
+ rm $NCBI_DIR/$bug.gbks
 done
