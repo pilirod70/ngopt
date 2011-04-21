f5bdc4d689e67cb8e9f2f197ee79d850b7e4e69b
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-04-21 20:34:20 +0000

    fixed calc_minlink.pl to output integers, not decimals

--- trunk/tools/gc-cov/calc_minlink.pl
+++ trunk/tools/gc-cov/calc_minlink.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
+use File::Basename;
+if (scalar(@ARGV)<2){
+	print "Usage: ".basename($0)." <insert> <read_length> <sam_file>\nif <sam_file> missing, read from stdin\n";
+	exit;
+}
 my $ins = shift;
 my $rdlen = shift;
 
@@ -12,11 +17,12 @@ while($line =~ m/^@.*LN:(\d+)/) {
 	$line = <>;
 	chomp $line;
 }
-
+print STDERR "genome length: $genome_length\n";
 my $nreads = 1;
 while($line = <>){
 	$nreads++;
 }
-
-my $minlink = $nreads*$ins/$genome_length;
+print STDERR "# reads: $nreads\n";
+my $minlink = $nreads*$ins/(2*$genome_length);
+printf("%.0f\n", $minlink);
 
