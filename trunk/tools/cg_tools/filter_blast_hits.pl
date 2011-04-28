288d8e319e284e44b32c575180e3266ed67cd2e8
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-04-28 00:12:49 +0000

    added more options for filtering blast output to filter_blast_hits.pl

--- trunk/tools/cg_tools/filter_blast_hits.pl
+++ trunk/tools/cg_tools/filter_blast_hits.pl
@@ -2,47 +2,87 @@
 use strict;
 use warnings;
 use File::Basename;
+use Getopt::Long;
 
-if (scalar(@ARGV) != 3){
-	print "Usage ".basename($0)." <min_len> <min_%id> <tabd_blast_file> \n";
+Getopt::Long::Configure(qw{no_auto_abbrev no_ignore_case_always});
+my $min_aln = 0; 
+my $min_pid = 0.0;
+my $min_bit = 0;
+my $max_eval = 100.0;
+my $max_mis = 100000000;
+my $max_gap = 100000000;
+my $no_reflexive = 0;
+my $help = 0;
+my $quiet = 0;
+my $numargs = scalar(@ARGV);
+GetOptions( 'alnlen|a=i' => \$min_aln,
+			'percid|p=f' => \$min_pid,
+			'bitscore|b=i' => \$min_bit,
+			'mismatch|m=i' => \$max_mis,
+			'eval|E=f' => \$max_eval,
+			'gap|g=i' => \$max_gap, 
+			'noreflex' => \$no_reflexive,
+			'quiet|q' => \$quiet,
+			'help|h' => \$help );
+
+
+
+if ($numargs == 0 || $help){
+	usage();
 	exit 1;
 }
-my $num_lines = 0;
-my $min_aln = shift;
-my $min_pid = shift;
-my $file = shift;
-open(IN,"<",$file);
-print STDERR "Removing blast hits less than $min_aln bp\n";
+summary() unless $quiet;
+
+
+#my $num_lines = 0;
+#my $min_aln = shift;
+#my $min_pid = shift;
+#my $file = shift;
+#open(IN,"<",$file);
+#print STDERR "Removing blast hits less than $min_aln bp\n";
 #    node0_0_0_93995	node0_0_0_93995	100.00	3815	0	0	3190	7004	3190	7004	0.0	7563
 #    node0_0_0_93995	node0_0_0_93995	100.00	2437	0	0	1	2437	1	2437	0.0	4831
-my $num_disc = 0;
-my $num_reflexive = 0;
-my $num_too_short = 0;
-my $num_too_diff = 0;
+#my $num_disc = 0;
+#my $num_reflexive = 0;
+#my $num_too_short = 0;
+#my $num_too_diff = 0;
 my %hits = ();
-while(<IN>) {
+while(<>) {
 	chomp;
 	my @hit = split;
-	$num_lines++;
-	if ($hit[3] >= $min_aln) {
-		if ($hit[0] eq $hit[1]) {
-			if ($hit[6] != $hit[8] || $hit[7] != $hit[9]) {
-			#	print $_."\n";
-				add_hit(\@hit);
-			} else {
-				$num_reflexive++;
-			}	
+#	$num_lines++;
+	if (okay(\@hit)) {
+		if ($no_reflexive){
+			if ($hit[0] eq $hit[1]) {
+				if ($hit[6] != $hit[8] || $hit[7] != $hit[9]) {
+					add_hit(\@hit);
+				} 
+			} 	
 		} else {
-			if ($hit[2] >= $min_pid) {
-			#	print $_."\n";
-				add_hit(\@hit);
-			} else {
-				$num_too_diff++;
-			}
-		}
-	} else { 
-		$num_too_short++;
+			add_hit(\@hit);
+		}	
+
 	}
+
+#	if ($hit[2] >= $min_pid && $hit[3] >= $min_aln) {
+#		if ($hit[0] eq $hit[1]) {
+#			if ($hit[6] != $hit[8] || $hit[7] != $hit[9]) {
+#			#	print $_."\n";
+#				add_hit(\@hit);
+#			} else {
+#				$num_reflexive++;
+#			}	
+#		} else {
+#			if ($hit[2] >= $min_pid) {
+#			#	print $_."\n";
+#				add_hit(\@hit);
+#			} else {
+#				$num_too_diff++;
+#			}
+#		}
+#	} else { 
+#		$num_too_short++;
+#	}
 }
 
 foreach my $ctg1 (keys %hits) {
@@ -53,14 +93,14 @@ foreach my $ctg1 (keys %hits) {
 	}
 } 
 
-print STDERR "Removed:\n";
-print STDERR "\t$num_reflexive reflexive hits\n";
-print STDERR "\t$num_too_short hits with less than $min_aln aligned base pairs\n";
-print STDERR "\t$num_too_diff hits with less than $min_pid percent identity\n";
+#print STDERR "Removed:\n";
+#print STDERR "\t$num_reflexive reflexive hits\n";
+#print STDERR "\t$num_too_short hits with less than $min_aln aligned base pairs\n";
+#print STDERR "\t$num_too_diff hits with less than $min_pid percent identity\n";
 
 sub add_hit {
 	my ($hit) = shift;
-	print STDERR "size = ".scalar(@$hit)." at line $num_lines\n";
+#	print STDERR "size = ".scalar(@$hit)." at line $num_lines\n";
 	my $ctg1 = shift @$hit;
 	my $ctg2 = shift @$hit;
 	if ($ctg2 lt $ctg1) {
@@ -102,4 +142,40 @@ sub add_hit {
 	}
 }
 
+sub okay {
+	my ($hit) = shift;
+	if ($hit->[2] >= $min_pid && $hit->[3] >= $min_aln && $hit->[4] <= $max_mis &&
+		$hit->[5] <= $max_gap && $hit->[10] <= $max_eval && $hit->[11] >= $min_bit) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
+
+sub usage {
+	print STDOUT "filter_blast_hits.pl [options] <m8_output>\n\n".
+				 "  options:\n".
+				 "   -a, --alnlen=<int>      minimum alignment length [0]\n".
+				 "   -p, --percid=<float>    minimum percent identity [0.0]\n".
+				 "   -b, --bitscore=<int>    minimum bitscore [0]\n".
+				 "   -m, --mismatch=<int>    maximum number of mismatches [100000000]\n".
+				 "   -E, --eval=<float>      maximum E-value [100.0]\n".
+				 "   -g, --gap=<int>         maximum number of gap openings [100000000]\n".
+				 "   --noreflex              discard reflexive hits\n".
+				 "   -h, --help              display this message\n\n".
+				 "   -q, --quiet             do not print status and summary messages\n".
+				 "if <m8_output> is absent, input will be read from stdin\n\n";
+
+}
+
+
+sub summary {
+	print "minimum alignment length: $min_aln\n";
+	print "minimum percent identity: $min_pid\n";
+	print "minimum bit score:        $min_bit\n";
+	print "maximum E-val:            $max_eval\n";
+	print "maximum no. mismatches:   $max_mis\n";
+	print "maximum no. gap openings: $max_gap \n";
+	print "keep reflexive hits?      ".($no_reflexive ? "no" : "yes")."\n";
+}
