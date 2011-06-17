1882c051b402673292819aabbb021c6ded66e3af
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-06-17 19:15:53 +0000

    fixed break_misassemblies.pl

--- trunk/bin/break_misassemblies.pl
+++ trunk/bin/break_misassemblies.pl
@@ -27,16 +27,17 @@ while(<IN>){
 	push(@{$blocks{$block}},\@ar);
 }
 
-my %breaks = ();
-print STDERR scalar(keys %blocks)." blocks \n";
-
+my %blkbnds = ();
+my $totblks = scalar(keys %blocks); 
+my $sigblks=0;
 # filter out short blocks
 for my $block (keys %blocks) {
 	my @ar = @{$blocks{$block}};
-	print STDERR "block $block has ".scalar(@ar)." points\n";
+	#print STDERR "block $block has ".scalar(@ar)." points\n";
 	if (scalar(@ar) < $min_blocks) {
 		delete($blocks{$block});
 	} else {
+		$sigblks++;
 		# calcate the boundaries of the segment we need to remove	
 		my $ctg1;
 		my $ctg2;
@@ -55,20 +56,25 @@ for my $block (keys %blocks) {
 			if ($pt2 =~ m/c(\d+)p(\d+)/) {
 				$pt2 = $2;
 			} 
+			# find the left-most and right-most boundaries in the two contigs
 			$ctg1_l = $pt1 if ($pt1 < $ctg1_l);
 			$ctg1_r = $pt1 if ($pt1 > $ctg1_r);
 			$ctg2_l = $pt2 if ($pt2 < $ctg2_l);
 			$ctg2_r = $pt2 if ($pt2 > $ctg2_r);
 		}
 		my @tmp1 = ($ctg1_l,$ctg1_r);
-		push(@{$breaks{$ctg1}},\@tmp1);
+		push(@{$blkbnds{$ctg1}},\@tmp1);
 
 		my @tmp2 = ($ctg2_l,$ctg2_r);
-		push(@{$breaks{$ctg2}},\@tmp2);
+		push(@{$blkbnds{$ctg2}},\@tmp2);
 	}
 }
-#print STDERR scalar(keys %breaks)."\n";
-#print STDERR join("\n",sort keys %breaks)."\n";
+
+
+
+print STDERR "Using $sigblks of $totblks blocks to break contigs\n";
+#print STDERR scalar(keys %blkbnds)."\n";
+#print STDERR join("\n",sort keys %blkbnds)."\n";
 
 open(IN,"<",$ctgLbl_file);
 my %contigs = ();
@@ -79,41 +85,75 @@ while(<IN>) {
 	$contigs{$name} = $id;
 }
 
+my %seqs = ();
 open(IN,"<",$fasta_file);
 my $ctg = "";
 my $seq = "";
 while (<IN>){
 	chomp;
 	if ($_ =~ m/^>(.*)/){
-#		print STDERR ">".$ctg."<".$contigs{$ctg}."\n";
-		unless (length($ctg) == 0 || !defined($breaks{$contigs{$ctg}})) {
-			my @ar = @{$breaks{$contigs{$ctg}}};
-			@ar = sort {$a->[0] <=> $b->[0]} @ar;
-			my $offset = 0;
-			my $sub_seq = 0;
-			for my $coord (@ar){
-				print STDERR "\$coord length = ".scalar(@$coord)."\n";
-				$sub_seq++;
-				my $l = $coord->[0];
-				my $r = $coord->[1];
-				print ">$ctg\_$sub_seq\n";
-				print substr($seq,$offset,$l-100)."\n";
-				$sub_seq++;
-				print ">$ctg\_$sub_seq\n";
-				print substr($seq,$l,$r-$l+1)."\n";
-				$offset = $r + 100; 
-			}
+		#print STDERR ">".$ctg."<".$contigs{$ctg}."\n";
+		unless (length($ctg) == 0) {
+			$seqs{$ctg} = $seq;
+			$seq = "";
 		}
 		$ctg = $1;
-		print $_."\n" unless(defined($breaks{$contigs{$ctg}}));
 	} else {
-		if (defined($breaks{$contigs{$ctg}})){
-			$seq .= $_; 
-		} else {
-			print $_."\n";
-		}
+		$seq .= $_; 
 	}
 	
 }
+$seqs{$ctg} = $seq;
 
+#my %breaks = ();
+#for $ctg 
+
+for $ctg (keys %seqs) {
+	if (defined($blkbnds{$contigs{$ctg}})){
+		my @ar = @{$blkbnds{$contigs{$ctg}}};
+		@ar = sort {$a->[0] <=> $b->[0]} @ar;
+		my $start = 0;
+		my $sub_seq = 0;
+		$seq = $seqs{$ctg};
+		for my $coord (@ar){
+			my $l = $coord->[0];
+			my $r = $coord->[1];
+			print STDERR "Breaking $ctg at ($l,$r)\n";
+			if ($l-100-$start > 100) {
+				$sub_seq++;
+				print ">$ctg\_$sub_seq\n";
+				print substr($seq,$start,$l-100-$start)."\n";
+			} 
+			if ($r-$l > 100) {
+				$sub_seq++;
+				print ">$ctg\_$sub_seq\n";
+				print substr($seq,$l,$r-$l)."\n";
+			}
+			$start = $r + 100; 
+		}
+	} else {
+		print ">$ctg\n".$seqs{$ctg}."\n";
+	}
+}
+
+
+#unless (length($ctg) == 0 || !defined($blkbnds{$contigs{$ctg}})) {
+#	my @ar = @{$blkbnds{$contigs{$ctg}}};
+#	@ar = sort {$a->[0] <=> $b->[0]} @ar;
+#	my $start = 0;
+#	my $sub_seq = 0;
+#	for my $coord (@ar){
+#		$sub_seq++;
+#		my $l = $coord->[0];
+#		my $r = $coord->[1];
+#		print ">$ctg\_$sub_seq\n";
+#		print substr($seq,$start,$l-100)."\n";
+#		$sub_seq++;
+#		print ">$ctg\_$sub_seq\n";
+#		print substr($seq,$l,$r-$l+1)."\n";
+#		$start = $r + 100; 
+#	}
+#}
+#$ctg = $1;
+#print $_."\n" unless(defined($blkbnds{$contigs{$ctg}}));
 
