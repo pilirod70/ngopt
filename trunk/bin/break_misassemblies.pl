057d800e6a05d5370a966b06315aa00696be23dd
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-06-22 23:13:49 +0000

    added directory for third party software and made some changes to fish-contig breaker script

--- trunk/bin/break_misassemblies.pl
+++ trunk/bin/break_misassemblies.pl
@@ -19,19 +19,26 @@ my %blocks = ();
 
 # read in our blocks
 open(IN,"<",$blocks_file);
-<IN>;
-while(<IN>){
-	chomp;
-	my @ar = split(/\t/,$_);
+my $line = <IN>;
+$line = <IN>;
+while(!($line =~ m/^-by size/)){
+	chomp $line;
+	my @ar = split(/\t/,$line);
 	my $block = $ar[2];
 	push(@{$blocks{$block}},\@ar);
+	$line = <IN>;
 }
+
+
 
 my %blkbnds = ();
 my $totblks = scalar(keys %blocks); 
 my $sigblks=0;
+
+my %cncts = ();
+
 # filter out short blocks
-for my $block (keys %blocks) {
+for my $block ( sort {$a <=> $b} (keys %blocks) ) {
 	my @ar = @{$blocks{$block}};
 	#print STDERR "block $block has ".scalar(@ar)." points\n";
 	if (scalar(@ar) < $min_blocks) {
@@ -45,40 +52,65 @@ for my $block (keys %blocks) {
 		my $ctg1_r = 0;
 		my $ctg2_l = 9**9**9;
 		my $ctg2_r = 0;
+		my $first =  1;
 		for my $point (@ar) {
 			$ctg1 = $point->[0];
 			$ctg2 = $point->[1];
-			my $pt1 = $point->[5];
-			if ($pt1 =~ m/c(\d+)p(\d+)/) {
-				$pt1 = $2;
-			} 
-			my $pt2 = $point->[6];
-			if ($pt2 =~ m/c(\d+)p(\d+)/) {
-				$pt2 = $2;
-			} 
-			# find the left-most and right-most boundaries in the two contigs
-			$ctg1_l = $pt1 if ($pt1 < $ctg1_l);
-			$ctg1_r = $pt1 if ($pt1 > $ctg1_r);
-			$ctg2_l = $pt2 if ($pt2 < $ctg2_l);
-			$ctg2_r = $pt2 if ($pt2 > $ctg2_r);
+			if ($first) {
+				$cncts{$ctg1} = () unless defined($cncts{$ctg1});
+				$cncts{$ctg1}{$ctg2} = 0 unless defined($cncts{$ctg1}{$ctg2});
+				$cncts{$ctg1}{$ctg2}++;	
+				$first = 0;
+			}
+			my @pt1 = split(/,/,$point->[5]);
+			for my $pt (@pt1){
+				if ($pt =~ m/c(\d+)p(\d+)/) {
+					$pt = $2;
+				} 
+				# find the left-most and right-most boundaries in the first contig
+				$ctg1_l = $pt if ($pt < $ctg1_l);
+				$ctg1_r = $pt if ($pt > $ctg1_r);
+			}
+			my @pt2 = split(/,/,$point->[6]);
+			for my $pt (@pt2){
+				if ($pt =~ m/c(\d+)p(\d+)/) {
+					$pt = $2;
+				} 
+				# find the left-most and right-most boundaries in the second contig
+				$ctg2_l = $pt if ($pt < $ctg2_l);
+				$ctg2_r = $pt if ($pt > $ctg2_r);
+			}
 		}
+		print STDERR "Block $block: $ctg1|$ctg1_l-$ctg1_r  $ctg2|$ctg2_l-$ctg2_r\n";
 		my @tmp1 = ($ctg1_l,$ctg1_r);
-		push(@{$blkbnds{$ctg1}},\@tmp1);
+		push(@{$blkbnds{$ctg1}},@tmp1);
 
 		my @tmp2 = ($ctg2_l,$ctg2_r);
-		push(@{$blkbnds{$ctg2}},\@tmp2);
+		push(@{$blkbnds{$ctg2}},@tmp2);
 	}
 }
 
+for my $c1 (sort keys %cncts){
+	for my $c2 (sort keys %{$cncts{$c1}}) {
+		print STDERR "$c1\t$c2\t".$cncts{$c1}{$c2}."\n";
+	}
+
+}
+
+for my $ctg (keys %blkbnds){
+	my $aref = $blkbnds{$ctg};
+	my @sorted = sort {$a <=> $b} @$aref;
+	delete($blkbnds{$ctg});
+	push(@{$blkbnds{$ctg}},@sorted);
+}
 
 
 print STDERR "Using $sigblks of $totblks blocks to break contigs\n";
-#print STDERR scalar(keys %blkbnds)."\n";
-#print STDERR join("\n",sort keys %blkbnds)."\n";
+print STDERR scalar(keys %blkbnds)." contigs have blocks\n";
 
+# make a map of contig-names to contig-ids
 open(IN,"<",$ctgLbl_file);
 my %contigs = ();
-
 while(<IN>) {
 	chomp;
 	my ($id,$name) = split(/\t/,$_);
@@ -89,71 +121,70 @@ my %seqs = ();
 open(IN,"<",$fasta_file);
 my $ctg = "";
 my $seq = "";
+my $nbases = 0;
 while (<IN>){
 	chomp;
 	if ($_ =~ m/^>(.*)/){
-		#print STDERR ">".$ctg."<".$contigs{$ctg}."\n";
 		unless (length($ctg) == 0) {
 			$seqs{$ctg} = $seq;
 			$seq = "";
 		}
 		$ctg = $1;
 	} else {
+		$nbases += length($_);
 		$seq .= $_; 
 	}
 	
 }
 $seqs{$ctg} = $seq;
 
-#my %breaks = ();
-#for $ctg 
-
+print STDERR "Found $nbases total bases\n";
+my $currbases = 0;
 for $ctg (keys %seqs) {
+	if (!defined ($contigs{$ctg})){
+		print ">$ctg\n".$seqs{$ctg}."\n";
+		$currbases += length($seqs{$ctg});
+		next;
+	}
 	if (defined($blkbnds{$contigs{$ctg}})){
 		my @ar = @{$blkbnds{$contigs{$ctg}}};
-		@ar = sort {$a->[0] <=> $b->[0]} @ar;
-		my $start = 0;
 		my $sub_seq = 0;
 		$seq = $seqs{$ctg};
-		for my $coord (@ar){
-			my $l = $coord->[0];
-			my $r = $coord->[1];
-			print STDERR "Breaking $ctg at ($l,$r)\n";
-			if ($l-100-$start > 100) {
+		if ($ar[0] >= 100) {
+			$sub_seq++;
+			print STDERR "Breaking $ctg at $ar[0]\n";
+			print ">$ctg|$sub_seq|1-$ar[0]\n";
+			print substr($seq,0,$ar[0])."\n";
+			$currbases += $ar[0];
+		} else {
+			print STDERR "Discarding region >$ctg|1-$ar[0]\n";
+		}	
+		for (my $i = 1; $i < scalar(@ar); $i++){
+			my $l = $ar[$i-1]+1;
+			my $r = $ar[$i];
+			if ($r - $l + 1 >= 100){
 				$sub_seq++;
-				print ">$ctg\_$sub_seq\n";
-				print substr($seq,$start,$l-100-$start)."\n";
-			} 
-			if ($r-$l > 100) {
-				$sub_seq++;
-				print ">$ctg\_$sub_seq\n";
-				print substr($seq,$l,$r-$l)."\n";
+				print STDERR "Breaking $ctg at $r\n";
+				print ">$ctg|$sub_seq|$l-$r\n";
+				print substr($seq,$l-1,$r-$l+1)."\n";
+				$currbases += $r-$l+1;
+			} else {
+				print STDERR "Discarding region >$ctg|$l-$r\n";
 			}
-			$start = $r + 100; 
 		}
-	} else {
-		print ">$ctg\n".$seqs{$ctg}."\n";
+		my $l = $ar[scalar(@ar)-1]+1;
+		my $r = length($seq);
+		if ($r - $l + 1 >= 100){
+			$sub_seq++;
+			print STDERR "Breaking $ctg at $l\n";
+			print ">$ctg|$sub_seq|$l-$r\n";
+			print substr($seq,$l-1,$r-$l+1)."\n";
+			$currbases += $r-$l+1;
+		} else {
+			print STDERR "Discarding region >$ctg|$l-$r\n";
+		}
+		
 	}
 }
-
-
-#unless (length($ctg) == 0 || !defined($blkbnds{$contigs{$ctg}})) {
-#	my @ar = @{$blkbnds{$contigs{$ctg}}};
-#	@ar = sort {$a->[0] <=> $b->[0]} @ar;
-#	my $start = 0;
-#	my $sub_seq = 0;
-#	for my $coord (@ar){
-#		$sub_seq++;
-#		my $l = $coord->[0];
-#		my $r = $coord->[1];
-#		print ">$ctg\_$sub_seq\n";
-#		print substr($seq,$start,$l-100)."\n";
-#		$sub_seq++;
-#		print ">$ctg\_$sub_seq\n";
-#		print substr($seq,$l,$r-$l+1)."\n";
-#		$start = $r + 100; 
-#	}
-#}
-#$ctg = $1;
-#print $_."\n" unless(defined($blkbnds{$contigs{$ctg}}));
+print STDERR "Printed $currbases total bases \n";
 
