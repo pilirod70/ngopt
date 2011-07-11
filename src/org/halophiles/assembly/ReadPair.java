23456784eb1ea8a906e8f8542edb219e43f41731
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-07-11 21:42:57 +0000

    fixed mean-insert calculation bug in the FISH input exporter. was forgetting to tack on the length of the read itself.

--- src/org/halophiles/assembly/ReadPair.java
+++ src/org/halophiles/assembly/ReadPair.java
@@ -4,10 +4,12 @@ public class ReadPair{
 	public final String hdr;
 	public int pos1 = 0;
 	public boolean rev1 = false;
+	public int len1 = 0;
 	public Contig ctg1;
 	public String[] sam1;
 	public int pos2 = 0;
 	public boolean rev2 = false;
+	public int len2 = 0;
 	public Contig ctg2;
 	public String[] sam2;
 	public boolean paired;
@@ -19,10 +21,11 @@ public class ReadPair{
 	public String contigString(){
 		return ctg1.name+"-"+ctg2.name;
 	}
-	public int addRead(int pos, boolean rev, Contig ctg, String[] sam){
+	public int addRead(int pos, boolean rev, int len, Contig ctg, String[] sam){
 		if (pos1 == 0){
 			pos1 = pos;
 			rev1 = rev;
+			len1 = len;
 			ctg1 = ctg;
 			sam1 = sam;
 			paired = false;
@@ -33,21 +36,25 @@ public class ReadPair{
 			if (ctg1.getId() < ctg.getId()){ // alphabetize for consistency
 				pos2 = pos;
 				rev2 = rev;
+				len2 = len;
 				ctg2 = ctg;
 				sam2 = sam;
 			} else if (ctg1.getId() == ctg.getId()){ 
 				if (pos1 > pos) { // order points for consistency
 					pos2 = pos1;
 					rev2 = rev1;
+					len2 = len1;
 					ctg2 = ctg1;
 					sam2 = sam1;
 					pos1 = pos;
 					rev1 = rev;
+					len1 = len;
 					ctg1 = ctg;
 					sam1 = sam;
 				} else {
 					pos2 = pos;
 					rev2 = rev;
+					len2 = len;
 					ctg2 = ctg;
 					sam2 = sam;				 
 				}
@@ -56,10 +63,12 @@ public class ReadPair{
 			} else {
 				pos2 = pos1;
 				rev2 = rev1;
+				len2 = len1;
 				ctg2 = ctg1;
 				sam2 = sam1;
 				pos1 = pos;
 				rev1 = rev;
+				len1 = len;
 				ctg1 = ctg;
 				sam1 = sam;
 			}
@@ -68,5 +77,12 @@ public class ReadPair{
 		} else {
 			return -1;
 		}
+	}
+	
+	public int getInsert(){
+		if (this.ctg1 != null && this.ctg2 != null && this.ctg1.equals(this.ctg2))
+			return this.pos2-this.pos1+this.len2;
+		else 
+			return -1;
 	}
 }
