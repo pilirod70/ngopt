6512292b33765b9e1b3581cdab66e2feee7913f8
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-07-12 18:52:51 +0000

    made changes to SAM getter scripts

--- trunk/tools/gc-cov/get_sam.sh
+++ trunk/tools/gc-cov/get_sam.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 3 ] ; then
 	echo "Usage: `basename $0` <output_dir> <ref> <reads>"
-	echo "Final pileup file is printed to stdout"
+	echo "Final sam file is printed to stdout"
 	exit
 fi
 
@@ -14,8 +14,8 @@ ref_base=`basename $ref`
 prefix=$out/`basename $ref`
 reads=$3
 base=`basename $reads`
-bwa index -a is -p $prefix $ref > $out/index.out
-bwa aln $prefix $reads 2> $out/aln.err | bwa samse $prefix - $reads 2> $out/samse.err  
+bwa index -a is -p $prefix $ref > /dev/null
+bwa aln $prefix $reads | bwa samse $prefix - $reads   
 
 #bwa aln $prefix $reads 2> $out/aln.err | bwa samse $prefix - $reads 2> $out/samse.err > $out/out.sam 
 #samtools view -bhS $out/out.sam 2> $out/view.err > $out/out.bam 
