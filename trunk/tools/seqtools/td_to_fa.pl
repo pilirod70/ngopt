c321bd87e89e4ec7b5931fd3e225e04f64cfa441
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-07-19 23:53:10 +0000

    convert tab-delimited to fasta

---
+++ trunk/tools/seqtools/td_to_fa.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+my %seen = ();
+
+while(<>){
+	chomp;
+	my ($hdr, $seq) = split /\t/ ;
+	if (defined($seen{$hdr})){
+		print "\@$hdr/2\n$seq\n";
+	} else {
+		print "\@$hdr/1\n$seq\n";
+		$seen{$hdr} = 1;
+	}	
+}
