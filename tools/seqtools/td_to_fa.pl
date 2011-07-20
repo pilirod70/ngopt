77d542eb9cb41edb325feb4809061bbca7d21a4a
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-07-20 16:54:25 +0000

    td_to_fa.pl was printing '@' in headers instead of '>'

--- tools/seqtools/td_to_fa.pl
+++ tools/seqtools/td_to_fa.pl
@@ -8,9 +8,9 @@ while(<>){
 	chomp;
 	my ($hdr, $seq) = split /\t/ ;
 	if (defined($seen{$hdr})){
-		print "\@$hdr/2\n$seq\n";
+		print "\>$hdr/2\n$seq\n";
 	} else {
-		print "\@$hdr/1\n$seq\n";
+		print "\>$hdr/1\n$seq\n";
 		$seen{$hdr} = 1;
 	}	
 }
