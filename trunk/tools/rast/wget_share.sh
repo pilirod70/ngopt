7c6ba93cf7aeb9070a1c0de1d33785206710fcee
aaron.darling <aaron.darling@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-07-27 11:51:48 +0000

    miscellaneous adjustments needed

--- trunk/tools/rast/wget_share.sh
+++ trunk/tools/rast/wget_share.sh
@@ -9,14 +9,14 @@ pass=$2
 email=$3
 jobid=$4
 
-cookie='mktemp'
-url=https://rast.nmpdr.org/rast.cgi?page=JobShare&email=$email&share_job=%20Share%20job%20with%20this%20user%20or%20group%20
+cookie=`mktemp`
+url=http://rast.nmpdr.org/rast.cgi\?page=JobShare\&email=$email\&action=share_job\&share_job=%20Share%20job%20with%20this%20user%20or%20group%20
 
 function share {
-	wget --load-cookie $cookie $url&job=$1
+	wget --load-cookie $cookie $url\&job=$1
 }
 
-wget --save-cookie $cookie http://rast.nmpdr.org/rast.cgi?login=$user&password=$pass&action=perform_login
+wget --save-cookie $cookie http://rast.nmpdr.org/rast.cgi\?login=$user\&password=$pass\&action=perform_login
 if [ -f $jobid ]; then
 	for id in `cat $jobid`; do
 		share $id
