e1ae1416d37305032d944e667f96f8a298c4d8bd
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-10-17 23:26:35 +0000

    cleaned up more imports to a5qc

--- branches/20111012_dbscan/src/org/halophiles/tools/SummaryStats.java
+++ branches/20111012_dbscan/src/org/halophiles/tools/SummaryStats.java
@@ -1,32 +1,6 @@
 package org.halophiles.tools;
 
-import java.util.Comparator;
-
-import jsc.correlation.KendallCorrelation;
-import jsc.correlation.SpearmanCorrelation;
-import jsc.datastructures.PairedData;
-
 public class SummaryStats {
-	
-	private static class IntegerSorter implements Comparator<Integer>{
-		public IntegerSorter(int[] dat){
-			this.dat = dat;
-		}
-		public int compare(Integer a, Integer b){
-			return dat[a.intValue()] - dat[b.intValue()];
-		}
-		private int[] dat;
-	}
-	
-	private static class DoubleSorter implements Comparator<Double>{
-		public DoubleSorter(double[] dat){
-			this.dat = dat;
-		}
-		public int compare(Double a, Double b){
-			return (int) (dat[a.intValue()] - dat[b.intValue()]);
-		}
-		private double[] dat;
-	}
 	
 	public static double mean(double[] dat) {
 		return sum(dat) / ((double)dat.length);
@@ -109,25 +83,7 @@ public class SummaryStats {
 			throw new IllegalArgumentException("dat1 and dat2 must be the same length");
 		return covariance(dat1, mean1, dat2, mean2)/Math.sqrt(variance(dat1,mean1)*variance(dat2,mean2));
 	}
-	
-	
-	public static double spearman(double[] dat1, double[] dat2){
-		if (dat1.length != dat2.length)
-			throw new IllegalArgumentException("dat1 and dat2 must be the same length");
-		return new SpearmanCorrelation(new PairedData(dat1,dat2)).getR();
-	}
-	
-	public static double kendall(double[] dat1, double[] dat2){
-		if (dat1.length != dat2.length)
-			throw new IllegalArgumentException("dat1 and dat2 must be the same length");
-		
-		if (dat1.length < 2)
-			throw new IllegalArgumentException("dat1 must contain at least 2 elements");
-		if (dat2.length < 2)
-			throw new IllegalArgumentException("dat2 must contain at least 2 elements");
-		return new KendallCorrelation(new PairedData(dat1,dat2)).getR();
-	}
-	
+
 	public static double covariance(int[] dat1, int[] dat2){
 		int mean1 = mean(dat1);
 		int mean2 = mean(dat2);
