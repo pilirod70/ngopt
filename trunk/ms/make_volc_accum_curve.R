a467cd14495372418ff120311baba6a73f0cca8b
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-11-16 00:23:36 +0000

    fixed quotes in a5 appnote latex. changed R script for making accumulation plots so that it uses appropriate data.

--- trunk/ms/make_volc_accum_curve.R
+++ trunk/ms/make_volc_accum_curve.R
@@ -1,39 +1,40 @@
 #!/usr/bin/env Rscript
 
 aco = sort(read.table("dat/Volc/A5/dsm3757.a5.contigs.len")[,1],decreasing=TRUE)
-acb = sort(read.table("dat/Volc/A5/dsm3757.a5.contigs.broken.len")[,1],decreasing=TRUE)
-acr.ec = sort(read.table("dat/Volc/A5/dsm3757.a5.ec.crude.scaffolds.len")[,1],decreasing=TRUE)
-afi.ec = sort(read.table("dat/Volc/A5/dsm3757.a5.ec.final.scaffolds.len")[,1],decreasing=TRUE)
+#acb = sort(read.table("dat/Volc/A5/dsm3757.a5.contigs.broken.len")[,1],decreasing=TRUE)
+#acr.ec = sort(read.table("dat/Volc/A5/dsm3757.a5.ec.crude.scaffolds.len")[,1],decreasing=TRUE)
+#afi.ec = sort(read.table("dat/Volc/A5/dsm3757.a5.ec.final.scaffolds.len")[,1],decreasing=TRUE)
 acr = sort(read.table("dat/Volc/A5/dsm3757.a5.crude.scaffolds.len")[,1],decreasing=TRUE)
 afi = sort(read.table("dat/Volc/A5/dsm3757.a5.final.scaffolds.len")[,1],decreasing=TRUE)
 sco = sort(read.table("dat/Volc/SOAP/volc_soap.contig.len")[,1],decreasing=TRUE)
 ssc = sort(read.table("dat/Volc/SOAP/volc_soap.scafSeq.len")[,1],decreasing=TRUE)
 
-pdf("volc_accum_plot.pdf")
-paste("volc_accum_plot.pdf")
+file = "volc_accum_plot.tiff"
+tiff(file)
+paste(file)
 
-col=rainbow(6)
+col=rainbow(5)
 col[2] = "darkorange"
 tmp = col[2]
-col[2] = col[3]
-col[3] = col[5]
-col[5] = tmp
+col[2] = col[4]
+col[4] = tmp
+#col[3] = col[5]
 #main="Sequence length accumulation",
 plot(cumsum(aco),type='l',col=col[1],xlab="N largest sequences ", ylab="No. of bases (Mb)",yaxt='n')
 lbl=c(0,1,2,3,4)
 at=lbl*1000000
 axis(2,at=at,labels=lbl)
-points(cumsum(acb),type='l',col=col[2])
-points(cumsum(acr.ec),type='l',col=col[3])
-points(cumsum(afi.ec),type='l',col=col[4])
-#points(cumsum(acr),type='l',col=col[3])
-#points(cumsum(afi),type='l',col=col[4])
-points(cumsum(sco),type='l',col=col[5])
-points(cumsum(ssc),type='l',col=col[6])
+#points(cumsum(acb),type='l',col=col[2])
+#points(cumsum(acr.ec),type='l',col=col[3])
+#points(cumsum(afi.ec),type='l',col=col[4])
+points(cumsum(acr),type='l',col=col[2])
+points(cumsum(afi),type='l',col=col[3])
+points(cumsum(sco),type='l',col=col[4])
+points(cumsum(ssc),type='l',col=col[5])
 #points(cumsum(acr.ec),type='l',col=col[7])
 #points(cumsum(afi.ec),type='l',col=col[8])
 
-names=c("A5 - ctg","A5 - ctg-br","A5 - scaf","A5 - scaf-QC","SOAP - ctg","SOAP - scaf")
+names=c("A5 - ctg","A5 - scaf","A5 - scaf-QC","SOAP - ctg","SOAP - scaf")
 
 legend("bottomright", legend=names,col=col,lty=1)
 
