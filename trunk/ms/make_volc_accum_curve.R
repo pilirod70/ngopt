39866d5bdcd6833c54a42fbbbcf3e4df8accfa54
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2011-11-16 18:44:31 +0000

    added PLoS LaTeX

--- trunk/ms/make_volc_accum_curve.R
+++ trunk/ms/make_volc_accum_curve.R
@@ -9,8 +9,8 @@ afi = sort(read.table("dat/Volc/A5/dsm3757.a5.final.scaffolds.len")[,1],decreasi
 sco = sort(read.table("dat/Volc/SOAP/volc_soap.contig.len")[,1],decreasing=TRUE)
 ssc = sort(read.table("dat/Volc/SOAP/volc_soap.scafSeq.len")[,1],decreasing=TRUE)
 
-file = "volc_accum_plot.tiff"
-tiff(file)
+file = "volc_accum_plot.pdf"
+pdf(file,width=14)
 paste(file)
 
 col=rainbow(5)
@@ -18,21 +18,16 @@ col[2] = "darkorange"
 tmp = col[2]
 col[2] = col[4]
 col[4] = tmp
-#col[3] = col[5]
-#main="Sequence length accumulation",
 plot(cumsum(aco),type='l',col=col[1],xlab="N largest sequences ", ylab="No. of bases (Mb)",yaxt='n')
+#plot(cumsum(ssc),type='l',col=col[5],xlab="N largest sequences ", ylab="No. of bases (Mb)",yaxt='n')
 lbl=c(0,1,2,3,4)
 at=lbl*1000000
 axis(2,at=at,labels=lbl)
-#points(cumsum(acb),type='l',col=col[2])
-#points(cumsum(acr.ec),type='l',col=col[3])
-#points(cumsum(afi.ec),type='l',col=col[4])
 points(cumsum(acr),type='l',col=col[2])
 points(cumsum(afi),type='l',col=col[3])
 points(cumsum(sco),type='l',col=col[4])
 points(cumsum(ssc),type='l',col=col[5])
-#points(cumsum(acr.ec),type='l',col=col[7])
-#points(cumsum(afi.ec),type='l',col=col[8])
+#points(cumsum(aco),type='l',col=col[1])
 
 names=c("A5 - ctg","A5 - scaf","A5 - scaf-QC","SOAP - ctg","SOAP - scaf")
 
