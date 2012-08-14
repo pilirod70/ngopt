b55ab48d2b3b21b93289c74f1f244470cc08ee57
andrew.j.tritt <andrew.j.tritt@3b2a58fe-1683-11df-9ff2-e3a8c39d1aea>
2012-08-14 17:20:06 +0000

    deleting deployment branch that was previously created. there are minor compile problems that need to be fixed

--- branches/jgi_misasm_det_release_v1.0/bin/readmap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#$ -cwd
-#$ -S /bin/bash
-#$ -t 1-45
-SCRATCH=/state/partition1/eisenlab/
-mkdir -p $SCRATCH
-/home/koadman/software/SHRiMP_1_2_1/bin/rmapper-ls -B reads.$SGE_TASK_ID $1 > $SCRATCH/reads.$SGE_TASK_ID.$1.mapping
-mv $SCRATCH/reads.$SGE_TASK_ID.$1.mapping .
